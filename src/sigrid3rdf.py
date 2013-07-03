630fa0e90396ee76d35ae352c9ca81ec3f29ba08
mccusker <mccusker@e908004f-b4f4-d9c9-901e-d6bd05ff0dc8>
2013-07-03 06:42:07 +0000

    added extra metadata, auto-reproject to lat/long

--- src/sigrid3rdf.py
+++ src/sigrid3rdf.py
@@ -2,6 +2,11 @@
 
 from rdflib import *
 from surf import *
+from lxml import html, etree
+import urllib
+import tarfile
+import tempfile
+import datetime
 
 ns.register(egg="http://purl.org/nsidc/jcomm/egg#")
 ns.register(wmo="http://purl.org/wmo/seaice#")
@@ -9,6 +14,8 @@ ns.register(sigrid="http://purl.org/nsidc/jcomm/sigrid#")
 ns.register(gml="http://www.opengis.net/def/dataType/OGC-GML/3.0/")
 ns.register(geo="http://www.opengis.net/ont/OGC-GeoSPARQL/1.0/")
 ns.register(ssiii="http://purl.org/nsidc/ssiii/seaice#")
+ns.register(sio="http://semanticscience.org/resource/")
+ns.register(prov="http://www.w3.org/ns/prov#")
 
 gmltype = ns.GML["GMLLiteral"]
 
@@ -25,6 +32,9 @@ def bindPrefixes(graph):
     graph.bind('gml', URIRef("http://www.opengis.net/def/dataType/OGC-GML/3.0/"))
     graph.bind('geo', URIRef("http://www.opengis.net/ont/OGC-GeoSPARQL/1.0/"))
     graph.bind('ssiii', URIRef("http://purl.org/nsidc/ssiii/seaice#"))
+    graph.bind('sio',URIRef("http://semanticscience.org/resource/"))
+    graph.bind('prov',URIRef("http://www.w3.org/ns/prov#"))
+    graph.bind('dc',URIRef("http://purl.org/dc/terms/"))
 
 def Usage():
     print('Usage: vec_tr.py infile outfile [layer]')
@@ -32,76 +42,151 @@ def Usage():
     sys.exit(1)
 
 def convert(infile, layer_name=None):
-    ds = ogr.Open( infile, update = 0 )
-    store = store = Store(reader="rdflib", writer="rdflib",
-                          rdflib_store='IOMemory')
+    shpFile, xmlFile = downloadAndExtract(infile)
+    ds = ogr.Open( shpFile, update = 0 )
+    graph = Graph()
+    layers = []
     if layer_name == None:
         for i in range(ds.GetLayerCount()):
-            layerToRDF(ds.GetLayer(i),store)
+            layers.append(layerToRDF(ds.GetLayer(i),graph))
     else:
-        layerToRDF(ds.GetLayerByName(layer_name),store)
+        layers.append(layerToRDF(ds.GetLayerByName(layer_name),graph))
     ds.Destroy()
 
-    model = store.reader.graph
-    bindPrefixes(model)
-    return model
+    bindPrefixes(graph)
+    getMetadata(xmlFile, graph, infile, layers)
+    return graph
 
-def layerToRDF(layer, store = None):
+def get_class(graph, classURI):
+    def fn(node):
+        if type(node) == str:
+            node = URIRef(node)
+        result = graph.resource(node)
+        result.add(ns.RDF['type'],classURI)
+        return result
+    return fn
+
+def layerToRDF(layer, graph = None):
     layerName = layer.GetName()
-    if store == None:
-        store = Store(reader="rdflib", writer="rdflib",
-                      rdflib_store='IOMemory')
-    session = Session(store)
-    Zone = session.get_class(ns.EGG['Zone'])
-    ZoneComponent = session.get_class(ns.SSIII['ZoneComponent'])
-    Geometry = session.get_class(ns.GEO['Geometry'])
-        
+    print layerName
+    if graph == None:
+        graph = Graph()
+    Zone = get_class(graph, ns.EGG['Zone'])
+    ZoneComponent = get_class(graph, ns.SSIII['ZoneComponent'])
+    Geometry = get_class(graph, ns.GEO['Geometry'])
+    DataGranule = get_class(graph, ns.SIO['data-granule'])
+
+    layerRDF = DataGranule('http://purl.org/ssiii/layers/'+layerName)
+    layerRDF.add(ns.DCTERMS['title'],Literal(layerName))
     feature = layer.GetNextFeature()
+
+    outSpatialRef = osr.SpatialReference()
+    outSpatialRef.SetWellKnownGeogCS( "WGS84" )
+    inSpatialRef = layer.GetSpatialRef()
+
     while feature is not None:
-        geom = feature.GetGeometryRef().ExportToGML()
-        zoneURI = '#'+layerName+"/zone/"+str(feature.GetFID())
+        #geo = feature.GetGeometryRef()
+        #geo.Transform(
+        geom = feature.GetGeometryRef()
+        coordTransform = osr.CoordinateTransformation(inSpatialRef, outSpatialRef)
+        geom.Transform(coordTransform)
+        gml = geom.ExportToGML()
+        
+        zoneURI = 'http://purl.org/ssiii/layers/'+layerName+"/zone/"+str(feature.GetFID())
         geometry = Geometry(zoneURI+"/geometry")
-        geometry.geo_asGML = Literal(geom,datatype=gmltype)
-        geometry.save()
+        geometry.set(ns.GEO['asGML'], Literal(gml,datatype=gmltype))
         zone = Zone(zoneURI)
-        zone.geo_hasGeometry = geometry
-        zone.sigrid3_hasSigrid3ConcentrationCode = feature.GetFieldAsString("CT")
-        zone.egg_hasComponent = []
+        zone.set(ns.SIO['is-member-of'],layerRDF.identifier)
+        zone.set(ns.GEO['hasGeometry'], geometry.identifier)
+        zone.set(ns.SIGRID['hasSigrid3ConcentrationCode'],
+                 Literal(feature.GetFieldAsString("CT")))
+        #zone.egg_hasComponent = []
         for comp in ['A','B','C']:
             if feature.IsFieldSet('C'+comp):
                 component = ZoneComponent(zoneURI+"/component/"+comp)
-                zone.egg_hasComponent.append(component)
-                component.sigrid_hasSigrid3ConcentrationCode = feature.GetFieldAsString('C'+comp)
+                zone.add(ns.EGG['hasComponent'], component.identifier)
+                component.set(ns.SIGRID['hasSigrid3ConcentrationCode'],
+                              Literal(feature.GetFieldAsString('C'+comp)))
                 if feature.IsFieldSet('S'+comp):
-                    component.sigrid_hasSigrid3DevelopmentStageCode = feature.GetFieldAsString('S'+comp)
+                    component.set(ns.SIGRID['hasSigrid3DevelopmentStageCode'],
+                                  Literal(feature.GetFieldAsString('S'+comp)))
                 if feature.IsFieldSet('F'+comp):
-                    component.sigrid_hasSigrid3IceFormCode = feature.GetFieldAsString('F'+comp)
-        if feature.IsFieldSet('CN') and feature.GetFieldAsString('CN')[0] != '-':
+                    component.set(ns.SIGRID['hasSigrid3IceFormCode'],
+                                  Literal(feature.GetFieldAsString('F'+comp)))
+        if feature.IsFieldSet('CN') and feature.GetFieldAsString('CN')[0] != '' and feature.GetFieldAsString('CN')[0] != '-':
             component = ZoneComponent(zoneURI+"/component/trace")
-            zone.egg_hasComponent.append(component)
-            component.sigrid_hasSigrid3ConcentrationCode = "01"
-            component.sigrid_hasSigrid3DevelopmentStageCode = feature.GetFieldAsString('CN')
-            component.save()
-        if feature.IsFieldSet('CD') and feature.GetFieldAsString('CD')[0] != '-':
+            zone.add(ns.EGG['hasComponent'],component.identifier)
+            component.add(ns.SIGRID['hasSigrid3ConcentrationCode'], Literal("01"))
+            component.add(ns.SIGRID['hasSigrid3DevelopmentStageCode'],
+                          Literal(feature.GetFieldAsString('CN')))
+        if feature.IsFieldSet('CD') and feature.GetFieldAsString('CD')[0] != '' and feature.GetFieldAsString('CD')[0] != '-':
             component = ZoneComponent(zoneURI+"/component/other")
-            zone.egg_hasComponent.append(component)
-            component.sigrid_hasSigrid3DevelopmentStageCode = feature.GetFieldAsString('CD')
-            component.save()
-        if feature.IsFieldSet('CF') and feature.GetFieldAsString('CF')[0] != '-':
+            zone.add(ns.EGG['hasComponent'],component.identifier)
+            component.add(ns.SIGRID['hasSigrid3DevelopmentStageCode'],
+                          Literal(feature.GetFieldAsString('CD')))
+        if feature.IsFieldSet('CF') and feature.GetFieldAsString('CF')[0] != '' and feature.GetFieldAsString('CF')[0] != '-':
             pcomponent = ZoneComponent(zoneURI+"/component/predominant")
-            pcomponent.rdf_type = ns.EGG['PredominantForm']
-            zone.egg_hasComponent.append(pcomponent)
-            pcomponent.sigrid_hasSigrid3DevelopmentStageCode = feature.GetFieldAsString('CD')[:2]
-            pcomponent.save()
+            pcomponent.add(ns.RDF['type'],ns.EGG['PredominantForm'])
+            zone.add(ns.EGG['hasComponent'],pcomponent.identifier)
+            pcomponent.add(ns.SIGRID['hasSigrid3DevelopmentStageCode'],
+                           Literal(feature.GetFieldAsString('CF')[:2]))
             scomponent = ZoneComponent(zoneURI+"/component/secondary")
-            scomponent.rdf_type = ns.EGG['SecondaryForm']
-            zone.egg_hasComponent.append(scomponent)
-            scomponent.sigrid_hasSigrid3DevelopmentStageCode = feature.GetFieldAsString('CD')[2:]
-            scomponent.save()
-        zone.save()
+            scomponent.add(ns.RDF['type'],ns.EGG['SecondaryForm'])
+            zone.add(ns.EGG['hasComponent'],scomponent.identifier)
+            scomponent.add(ns.SIGRID['hasSigrid3DevelopmentStageCode'],
+                           Literal(feature.GetFieldAsString('CF')[2:]))
         
         feature.Destroy()
         feature = layer.GetNextFeature()
+    return layerRDF
+
+def downloadAndExtract(url):
+    d = tempfile.mkdtemp()
+    tarFile = d+"/file.tar"
+    urllib.urlretrieve(url,tarFile)
+    tarball = tarfile.open(tarFile)
+    tarball.extractall(d)
+    fileNames = tarball.getnames()
+    xmlName = [d+"/"+f for f in fileNames if f.endswith(".xml")][0]
+    shpName = [d+"/"+f for f in fileNames if f.endswith(".shp")][0]
+    return shpName, xmlName
+
+def getMetadata(xmlFile, graph, url, layers):
+    dataset = graph.resource(URIRef('http://dx.doi.org/10.7265/N51V5BW9'))
+    dataset.set(ns.RDF['type'],ns.SIO['data-set'])
+    dataGranule = graph.resource(URIRef(url))
+    dataGranule.set(ns.RDF['type'],ns.SIO['data-granule'])
+    dataGranule.set(ns.SIO['is-member-of'],dataset.identifier)
+    doc = etree.parse(xmlFile)
+    abstract = doc.find('idinfo/descript/abstract').text
+    dataGranule.set(ns.DCTERMS['abstract'],Literal(abstract))
+    pubDate = doc.find('idinfo/citation/citeinfo/pubdate').text
+    pubDate = datetime.datetime.strptime(pubDate,'%Y%m%d').date()
+    effectiveDate = doc.find('idinfo/timeperd/timeinfo/sngdate/caldate').text
+    effectiveDate = datetime.datetime.strptime(effectiveDate,'%Y%m%d').date()
+    dataGranule.set(ns.PROV['generatedAtTime'],Literal(effectiveDate))
+    attribution = doc.find('idinfo/citation/citeinfo/origin').text
+    attributionURI = 'http://purl.org/ssiii/source/'+attribution.replace(" ","_").replace("(","").replace(")","")
+    Organization = get_class(graph, ns.PROV['Organization'])
+    source = Organization(attributionURI)
+    source.set(ns.RDFS['label'],Literal(attribution))
+    dataGranule.set(ns.PROV['wasAttributedTo'],source.identifier)
+    region = URIRef('http://purl.org/nsidc/cis/Sea-Ice-Chart-Regions#'
+                    +url.split('/')[7].replace("_",""))
+    dataGranule.set(ns.SIO['is-model-of'],region)
+    for layer in layers:
+        layer.add(ns.PROV['wasDerivedFrom'],dataGranule.identifier)
+        layer.add(ns.PROV['alternateOf'],dataGranule.identifier)
+        dataGranule.add(ns.DCTERMS['title'],layer.value(ns.DCTERMS['title']))
+        layer.set(ns.PROV['wasAttributedTo'],source.identifier)
+        layer.set(ns.SIO['is-model-of'],region)
+
+    #for accessionElement in doc.findall('//experiment/accession'):
+    #    accession = accessionElement.text.strip()
+    #    obj = HarvestObject(guid=accession, job=harvest_job, content=accession)
+    #    print "ArrayExpress accession: "+accession
+    #    obj.save()
+
 
 if __name__ == '__main__':
     infile = None
